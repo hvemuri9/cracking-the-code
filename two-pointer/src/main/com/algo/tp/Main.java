c44594ca7436270518d325dfe9b6a57cedb817f0
hvemuri99 <hemasree.vemuri@gmail.com>
2026-04-01 11:52:11 -0500

    Three pointers- DutchNationalFlag

--- two-pointer/src/main/com/algo/tp/Main.java
+++ two-pointer/src/main/com/algo/tp/Main.java
@@ -18,5 +18,9 @@ public class Main {
 
         //ReverseArray
         ReverseArray.reverse(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
+
+        //DutchNationalFlag
+        DutchNationalFlag.sort012(new int[]{2, 0, 2, 1, 1, 0});
+        DutchNationalFlag.sort012(new int[]{2, 0, 1});
     }
 }
